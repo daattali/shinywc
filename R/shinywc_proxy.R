93cfebf7f5fa040b7e461948c112901eb2f56c6c
Dean Attali <daattali@gmail.com>
2021-05-12 15:17:23 -0400

    proxy: don't expose all the internal get/set functions; change proxy$id to be a field instead of a function

--- R/shinywc_proxy.R
+++ R/shinywc_proxy.R
@@ -7,34 +7,7 @@ ShinywcProxy <- R6::R6Class(
     .id = NULL,
     .id_noNS = NULL,
     .session = NULL,
-    .attributes = list()
-  ),
-
-  public = list(
-    initialize = function(tag, id, session) {
-      if (is.null(session)) {
-        stop(tag, " proxy Can only be initialized in a Shiny environment", call. = FALSE)
-      }
-      private$.tag <- tag
-      private$.session <- session
-      private$.id_noNS <- id
-      private$.id <- private$.session$ns(private$.id_noNS)
-
-      private$.session$sendCustomMessage('shinywc-init-component', list(
-        id = private$.id
-      ))
-
-      shiny::observeEvent(private$.session$input[[paste0(private$.id_noNS, "_", private$.tag, "-attr-change")]], {
-        evt <- private$.session$input[[paste0(private$.id_noNS, "_", private$.tag, "-attr-change")]]
-        attr_name <- names(evt[1])
-        attr_val <- evt[[1]]
-        private$.attributes[[attr_name]] <- attr_val
-      })
-    },
-
-    id = function() {
-      private$.id_noNS
-    },
+    .attributes = list(),
 
     set_attr = function(attr, value) {
       private$.session$sendCustomMessage('shinywc-attr-set', list(
@@ -82,6 +55,36 @@ ShinywcProxy <- R6::R6Class(
 
     listen_event = function(event) {
       private$.session$input[[paste0(private$.id_noNS, "_event_", event)]]
+    }
+  ),
+
+  public = list(
+
+    initialize = function(tag, id, session) {
+      if (is.null(session)) {
+        stop(tag, " proxy Can only be initialized in a Shiny environment", call. = FALSE)
+      }
+      private$.tag <- tag
+      private$.session <- session
+      private$.id_noNS <- id
+      private$.id <- private$.session$ns(private$.id_noNS)
+
+      private$.session$sendCustomMessage('shinywc-init-component', list(
+        id = private$.id
+      ))
+
+      shiny::observeEvent(private$.session$input[[paste0(private$.id_noNS, "_", private$.tag, "-attr-change")]], {
+        evt <- private$.session$input[[paste0(private$.id_noNS, "_", private$.tag, "-attr-change")]]
+        attr_name <- names(evt[1])
+        attr_val <- evt[[1]]
+        private$.attributes[[attr_name]] <- attr_val
+      })
+    }
+  ),
+
+  active = list(
+    id = function() {
+      private$.id_noNS
     }
   )
 )
